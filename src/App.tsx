136acec34eb539a5ab995dd967505275fbeabb80
Ignatious <ignatiousr80@gmail.com>
2026-04-15 16:06:28 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import Layout from './components/Layout'
-import Hero from './components/Hero'
-import Clients from './components/Clients'
-import Expertises from './components/Expertises'
-import Work from './components/Work'
-import About from './components/About'
-import './App.css'
+import Layout from "./components/Layout";
+import Hero from "./components/Hero";
+import "./App.css";
 
 function App() {
   return (
     <Layout>
       <Hero />
-      <Clients />
-      <Expertises />
-      <Work />
-      <About />
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import './Layout.css';
+import React from "react";
+import Header from "./Header";
+import "./Layout.css";
+import Footer from "./Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,9 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="layout">
       <Header />
-      <main className="main-content">
-        {children}
-      </main>
+      <main className="main-content">{children}</main>
       <Footer />
     </div>
   );
